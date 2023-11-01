85a074ca5d307bbcf931a6dddfe05e63503a7a4a
Andon-LaFreniere <138069192+Andon-LaFreniere@users.noreply.github.com>
2023-10-31 23:51:05 -0400

    Create views.py

---
+++ views.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, render_template
+
+views = Blueprint(__name__, "views")
+
+@views.route("/")
+def home():
+    return render_template("index.html")
+
+@views.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+@views.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@views.route("/news")
+def news():
+    return render_template("news.html")
